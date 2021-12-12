322fc925a5af314ba96ea870fa49da82c5c07b90
“Radik <radik.zainullin@gmail.com>
2021-12-12 16:27:59 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-function sum(a, b) {
-  return a + b;
+function traversal(matrix) {
+  const result = [];
+  // берем первый элемент и запихиваем в массив
+  for (let i = 0; i < matrix.length; i++ ){
+      for (let j = 0; j < matrix[i].length; j++) {
+          result.push(matrix[i][j]);
+      }
+  }
+  return result;
 }
 
 module.exports = {
-  sum,
+  traversal,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,118 @@
 const { assert } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { traversal } = require('../src');
 
-describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+describe('Обход массива слева-направо, сверху-вниз', () => {
+  // it('Пустая матрица', () => {
+  //   const matrix = [[]];
+  //   const result = [];
+    
+  //   assert.deepEqual(traversal(matrix), result);
+  // });
+
+  describe('Квадратная матрица', () => {
+    it('Матрица 1x1', () => {
+      const matrix = [[1]];
+      const result = [1];
+      console.log("traversal(matrix) = ", traversal(matrix));
+      assert.deepEqual(traversal(matrix), result);
+    });
+
+    it('Матрица 3x3', () => {
+      const matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+      ];
+
+      const result = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+      assert.deepEqual(traversal(matrix), result);
+    });
+  });
+
+  describe('Матрица в одну строчку', () => {
+    it('Матрица 5x1', () => {
+      const matrix = [[1, 2, 3, 4, 5]];
+      const result = [1, 2, 3, 4, 5];
+
+      assert.deepEqual(traversal(matrix), result);
+    });
+
+    it('Матрица 10x1', () => {
+      const matrix = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]];
+      const result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+      assert.deepEqual(traversal(matrix), result);
+    });
+  });
+
+  describe('Матрица в один столбец', () => {
+    it('Матрица 1x5', () => {
+      const matrix = [[1], [2], [3], [4], [5]];
+      const result = [1, 2, 3, 4, 5];
+
+      assert.deepEqual(traversal(matrix), result);
+    });
+
+    it('Матрица 1x10', () => {
+      const matrix = [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]];
+      const result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+      assert.deepEqual(traversal(matrix), result);
+    });
+  });
+
+  describe('Не квадртаная матрица', () => {
+    it('Матрица 2x3', () => {
+      const matrix = [
+        [1, 2],
+        [3, 4],
+        [5, 6],
+      ];
+
+      const result = [1, 2, 3, 4, 5, 6];
+      assert.deepEqual(traversal(matrix), result);
+    });
+
+    it('Матрица 3x2', () => {
+      const matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+      ];
+
+      const result = [1, 2, 3, 4, 5, 6];
+      assert.deepEqual(traversal(matrix), result);
+    });
+
+    it('Матрица 5x6', () => {
+      // prettier-ignore
+      const matrix = [
+                [ 1,  2,  3,  4,  5],
+                [ 6,  7,  8,  9, 10],
+                [11, 12, 13, 14, 15],
+                [16, 17, 18, 19, 20],
+                [21, 22, 23, 24, 25],
+                [26, 27, 28, 29, 30],
+  		];
+
+      // prettier-ignore
+      const result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+      assert.deepEqual(traversal(matrix), result);
+    });
+  });
+
+  it('Финальный тест', () => {
+    const matrix = [
+      [8, 0, 5, 5, 8],
+      [8, 2, 8, 7, 0],
+      [8, 0, 5, 2, 5],
+      [1, 8, 1, 2, 6],
+    ];
+
+    // prettier-ignore
+    const result = [8, 0, 5, 5, 8, 8, 2, 8, 7, 0, 8, 0, 5, 2, 5, 1, 8, 1, 2, 6]
+    assert.deepEqual(traversal(matrix), result);
   });
 });
