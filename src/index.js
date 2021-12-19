436307ed84eed0fbc163d7d3ad79b1e673d73a79
“Radik <radik.zainullin@gmail.com>
2021-12-19 21:07:27 +0300

    takeV1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-// Напишите функцию take, которая принимает массив и возвращает новый массив, 
-// являющийся копией исходного массива. 
+// Напишите функцию take, которая принимает массив и возвращает новый массив,
+// являющийся копией исходного массива.
 // Не используйте методы массива, кроме push, shift, pop, unshift.
 
 function take() {
-  return null;
+  const inArr = arguments[0];
+  const outArr = [];
+  inArr.forEach((el) => {
+    outArr.push(el);
+  });
+
+  return outArr;
 }
 
 module.exports = {
--- test/index.spec.js
+++ test/index.spec.js
@@ -4,32 +4,37 @@ const { take } = require('../src');
 
 describe('Тестирование функции take', () => {
   it('Базовый тест', () => {
-    expect(take([])).toEqual([]);
-    expect(take([1, 2, 3])).toEqual([1, 2, 3]);
+    assert.deepEqual(take([]), []);
+
+    assert.deepEqual(take([1, 2, 3]), [1, 2, 3]);    
 
     const arr = [1, 2, 3];
-    expect(take(arr) === arr).toBe(false);
+    expect(take(arr) === arr).to.equal(false);
   });
 
   it('Пустой массив', () => {
     const array = [];
     const copy = take(array);
-
-    expect(copy).toEqual(array);
-    expect(copy === array).toBe(false);
+    assert.deepEqual(copy, array); 
+    expect(copy === array).to.equal(false);
   });
 
   it('Массив с элементами', () => {
     const array = [1, 7, -1, -100, 2];
     const copy = take(array);
 
-    expect(copy).toEqual(array);
-    expect(copy === array).toBe(false);
+    assert.deepEqual(copy, array); 
+
+    expect(copy === array).to.equal(false);
+    
   });
 
   it('Тест на иммутабельность', () => {
     const array = [1, 2, 3, 1, 2, 3];
     take(array);
-    expect(array).toEqual([1, 2, 3, 1, 2, 3]);
+
+    
+    assert.deepEqual(array, [1, 2, 3, 1, 2, 3]); 
+        
   });
 });
