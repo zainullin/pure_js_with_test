75f48a87136284acce00a3ca02768097be9b397a
“Radik <radik.zainullin@gmail.com>
2021-12-20 14:18:25 +0300

    задача решена

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-function sum(a, b) {
-  return a + b;
+// Напишите функцию createGeometricRow так, чтобы она возвращала
+// массив геометрической прогрессии размером size.
+function createGeometricRow(b1, q, size) {
+  const result = [];
+  for (let i = 0; i < size; i += 1) {
+    result.push(b1 * q ** i);
+  }
+  return result;
 }
 
 module.exports = {
-  sum,
+  createGeometricRow,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,21 @@
 const { assert } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { createGeometricRow } = require('../src');
 
 describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+  it("Тестирование функции createGeometricRow", () => {
+    assert.deepEqual(createGeometricRow(1, 0, 1), [1]);
+    assert.deepEqual(createGeometricRow(1, 0, 3), [1, 0, 0]);
+    assert.deepEqual(createGeometricRow(1, 0, 5), [1, 0, 0, 0, 0]);
+    assert.deepEqual(createGeometricRow(1, 7, 1), [1]);
+    assert.deepEqual(createGeometricRow(1, 7, 3), [1, 7, 49]);
+    assert.deepEqual(createGeometricRow(1, 7, 5), [1, 7, 49, 343, 2401]);
+    assert.deepEqual(createGeometricRow(1, 13, 1), [1]);
+    assert.deepEqual(createGeometricRow(1, 13, 3), [1, 13, 169]);
+    assert.deepEqual(createGeometricRow(1, 13, 5), [1, 13, 169, 2197, 28561]);
+    assert.deepEqual(createGeometricRow(-3, -2, 1), [-3]);
+    assert.deepEqual(createGeometricRow(-3, -2, 3), [-3, 6, -12]);
+    assert.deepEqual(createGeometricRow(-3, -2, 5), [-3, 6, -12, 24, -48]);
   });
 });
