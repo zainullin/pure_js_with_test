6bb6dfccd13b39a2e6109dd451c5299a56eb8af7
“Radik <radik.zainullin@gmail.com>
2021-12-20 15:37:48 +0300

    Задача решена

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-function sum(a, b) {
-  return a + b;
+// Функция getMaxitem принимает в качестве единственного аргумента не пустой массив чисел.
+// Функция должна вернуть элемент с максимальный значением.
+
+function getMaxItem(array) {
+  let max = Number.NEGATIVE_INFINITY;
+  for (let i = 0; i < array.length; i += 1) {
+    if (max < array[i]) max = array[i];
+  }
+  return max;
 }
 
 module.exports = {
-  sum,
+  getMaxItem,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,26 @@
-const { assert } = require('chai');
+const { expect } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { getMaxItem } = require('../src');
 
 describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+  it("Тестирование функции getMaxItem", () => {
+    const numbers1 = [1, 2, 3];
+    expect(getMaxItem(numbers1)).to.equal(3);
+    // expect(numbers1).toEqual([1, 2, 3]);
+  
+    const numbers2 = [6, 8, 9, 2, 4, 5];
+
+    expect(getMaxItem(numbers2)).to.equal(9);
+    // expect(numbers2).toEqual([6, 8, 9, 2, 4, 5]);
+  
+    const numbers3 = [6];
+
+    expect(getMaxItem(numbers3)).to.equal(6);
+    // expect(numbers3).toEqual([6]);
+  
+    const numbers4 = [8, 8, 8, 8];
+    expect(getMaxItem(numbers4)).to.equal(8);
+    // expect(numbers4).toEqual([8, 8, 8, 8]);
   });
 });
