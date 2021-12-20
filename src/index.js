f3299fe2e30dc6e361a32d6f2d2bd1ad5bd77028
“Radik <radik.zainullin@gmail.com>
2021-12-20 17:13:41 +0300

    задача решена

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-function sum(a, b) {
-  return a + b;
+function getMinItem(array) {
+  let min = Number.POSITIVE_INFINITY;
+  for (let i = 0; i < array.length; i += 1) {
+    if (min > array[i]) min = array[i];
+  }
+  return min;
 }
 
 module.exports = {
-  sum,
+  getMinItem,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,24 @@
-const { assert } = require('chai');
+const { expect } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { getMinItem } = require('../src');
 
 describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+  it("Тестирование функции getMinItem", () => {
+    const numbers1 = [1, 2, 3];
+    expect(getMinItem(numbers1)).to.equal(1);
+    // expect(numbers1).toEqual([1, 2, 3]);
+  
+    const numbers2 = [6, 8, 9, 2, 4, 5];
+    expect(getMinItem(numbers2)).to.equal(2);
+    // expect(numbers2).toEqual([6, 8, 9, 2, 4, 5]);
+  
+    const numbers3 = [6];
+    expect(getMinItem(numbers3)).to.equal(6);
+    // expect(numbers3).toEqual([6]);
+  
+    const numbers4 = [8, 8, 8, 8];
+    expect(getMinItem(numbers4)).to.equal(8);
+    // expect(numbers4).toEqual([8, 8, 8, 8]);
   });
 });
