8e11ab2acacf5358bb629bc348bb10383f82660b
“Radik <radik.zainullin@gmail.com>
2021-12-20 13:46:32 +0300

    задача решена

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-function sum(a, b) {
-  return a + b;
+// Напишите функцию getGeometricProgression так, чтобы она возвращала сумму 
+// членов арифметической прогрессии начиная с fromN и заканчивая toN порядковыми номерами.
+function getGeometricProgression(b1, q, fromN, toN) {
+  if (q === 1) {
+    return b1 * (toN - fromN + 1);
+  }
+  const bFromN = b1 * q ** (fromN - 1);
+  const result = (bFromN * (1 - q ** (toN - fromN + 1))) / (1 - q);
+
+  return result;
 }
 
 module.exports = {
-  sum,
+  getGeometricProgression,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,14 @@
-const { assert } = require('chai');
+const { expect } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { getGeometricProgression } = require('../src');
 
 describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+  it("Тестирование функции getGeometricProgression", () => {
+    expect(getGeometricProgression(1, 3, 3, 5)).to.equal(117);
+    expect(getGeometricProgression(2, 1, 5, 9)).to.equal(10);
+    expect(getGeometricProgression(1, -1, 3, 8)).to.equal(0);
+    expect(getGeometricProgression(1024, 0.5, 6, 8)).to.equal(56);
+    expect(getGeometricProgression(2048, -0.5, 2, 4)).to.equal(-768);
   });
 });
