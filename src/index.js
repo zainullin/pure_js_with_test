cf68a94a616d41de315757fd9a805bc6284a28d7
“Radik <radik.zainullin@gmail.com>
2021-12-21 13:16:08 +0300

    задача еще не решена

--- src/index.js
+++ src/index.js
@@ -1,7 +1,49 @@
-function sum(a, b) {
-  return a + b;
+// Функция getSumArray принимает в качестве аргументов любое количество массивов.
+// Все массивы численные, но могут быть разных размеров.
+// Напишите функцию getSumArray, которая возвращает массив, каждый элемент которой
+// является суммой элементов входных массивов на том же индексе.
+
+function getSumArray() {
+  // получаем массив входных значений
+  const arr = Array.from(arguments);
+  // проходим по входному массиву, элементами являются тоже массивы
+  // находим самый длинный, его длина нам нужна
+  let max = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (max < arr[i].length) {
+      max = arr[i].length;
+    }
+  }
+  // преобразуем массив arr, сделаем каждый элемент этого массива (а это тоже массив)
+  // одинакового размера - недостающий элемент = 0, длина каждого элемента равно max
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = 0; j < max; j += 1) {
+      if (arr[i][j] === undefined) {
+        arr[i][j] = 0;
+      }
+    }
+  }
+  console.log(arr);
+  // Сделаем пустой результирующий массив размером max
+  const result = [];
+  for (let p = 0; p < max; p += 1) {
+    result.push(0);
+  }
+  // заполним result
+  for (let i = 0; i < result.length; i += 1) {
+    result[i] = getResult(i, arr);
+  }
+  console.log(result);
+  return result;
+}
+function getResult(i, arr) {
+  let sum = 0;
+  for (let j = 0; j < arr.length; j += 1) {
+    sum += arr[j][i];
+  }
+  return sum;
 }
 
 module.exports = {
-  sum,
+  getSumArray,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,74 @@
 const { assert } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { getSumArray } = require('../src');
 
-describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+describe('Тестирование функции getSumArray', () => {
+  it('Тест 0', () => {
+    assert.deepEqual(getSumArray([0], [1, 2], [5]), [6, 2]);
+  });
+
+  it('Тест 1', () => {
+    const array1 = [0, 1, 2];
+
+    const result = [0, 1, 2];
+    assert.deepEqual(getSumArray(array1), result);
+    // expect(array1).toEqual([0, 1, 2]);
+  });
+
+  it('Тест 2', () => {
+    const array1 = [5, 5, 7, 7, 8];
+    const result = [5, 5, 7, 7, 8];
+    assert.deepEqual(getSumArray(array1), result);
+
+    // expect(array1).toEqual([5, 5, 7, 7, 8]);
+  });
+
+  it('Тест 3', () => {
+    const array1 = [0, 1, 2, 3, 4];
+    const array2 = [0, -1, -2, -3, -4];
+
+    const result = [0, 0, 0, 0, 0];
+
+    assert.deepEqual(getSumArray(array1, array2), result);
+    // expect(array1).toEqual([0, 1, 2, 3, 4]);
+    // expect(array2).toEqual([0, -1, -2, -3, -4]);
+  });
+
+  it('Тест 4', () => {
+    const array1 = [];
+    const array2 = [1];
+    const array3 = [2, 2];
+    const array4 = [3, 3, 3];
+    const array5 = [4, 4, 4, 4];
+    const array6 = [5, 5, 5, 5, 5];
+
+    const result = [15, 14, 12, 9, 5];
+    assert.deepEqual(getSumArray(array1, array2, array3, array4, array5, array6), result);
+    // expect(array1).toEqual([]);
+    // expect(array2).toEqual([1]);
+    // expect(array3).toEqual([2, 2]);
+    // expect(array4).toEqual([3, 3, 3]);
+    // expect(array5).toEqual([4, 4, 4, 4]);
+    // expect(array6).toEqual([5, 5, 5, 5, 5]);
+  });
+
+  it('Тест 5', () => {
+    const array1 = [5, 5, -21];
+    const array2 = [28, -27, -16, -48];
+    const array3 = [30, -21, -22, -44, -47, 45, -6, 6];
+    const array4 = [-21, 13, -14, -27, 23, -7];
+    const array5 = [46, 20];
+    const array6 = [-39, -8, -7, 29, -22, 25, -16];
+
+    const result = [49, -18, -80, -90, -46, 63, -22, 6];
+
+    assert.deepEqual(getSumArray(array1, array2, array3, array4, array5, array6), result);
+    // expect(array1).toEqual([5, 5, -21]);
+    // expect(array2).toEqual([28, -27, -16, -48]);
+    // expect(array3).toEqual([30, -21, -22, -44, -47, 45, -6, 6]);
+    // expect(array4).toEqual([-21, 13, -14, -27, 23, -7]);
+    // expect(array5).toEqual([46, 20]);
+    // expect(array6).toEqual([-39, -8, -7, 29, -22, 25, -16]);
   });
 });
