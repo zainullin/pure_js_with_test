f755525d07445021a7985bf9f6a115d6df22a137
“Radik <radik.zainullin@gmail.com>
2021-12-23 21:26:07 +0300

    задача решена

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-function sum(a, b) {
-  return a + b;
+// Напишите функцию take, которая принимает массив и
+// возвращающую массив из первых n элементов в том же порядке,
+// в котором элементы стоят в исходном массиве.
+// Если n не указан, то равен длине массива.
+// Не используйте методы массива, кроме push, shift, pop, unshift.
+
+function take(...args) {
+  // Получим аргументы
+  const inArray = args[0];
+  const n = args[1] === undefined || args[1] >= inArray.length ? inArray.length : args[1];
+
+  const result = [];
+  for (let i = 0; i < n; i += 1) {
+    result.push(inArray[i]);
+  }
+  return result;
 }
 
 module.exports = {
-  sum,
+  take,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,50 @@
 const { assert } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { take } = require('../src');
 
-describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+describe('Тестирование функции take', () => {
+  it('Базовый тест', () => {
+    assert.deepEqual(take([]), []);
+    assert.deepEqual(take([1, 2, 3]), [1, 2, 3]);
+    assert.deepEqual(take([1, 2, 3], 100), [1, 2, 3]);
+    assert.deepEqual(take([1, 2, 3, 4, 5], 1), [1]);
+    assert.deepEqual(take([1, 2, 3, 4, 5], 3), [1, 2, 3]);
+  });
+
+  it('Пустой массив', () => {
+    const array = [];
+    const copy = take(array, 0);
+
+    assert.deepEqual(copy, array);
+    // console.log(copy === array);
+    assert.notStrictEqual(copy, array);
+    // expect(copy === array).toBe(false);
+  });
+
+  it('Массив с 5 элементами', () => {
+    const array = [1, 7, -1, -100, 2];
+
+    assert.deepEqual(take(array), [1, 7, -1, -100, 2]);
+    assert.deepEqual(take(array, 5), [1, 7, -1, -100, 2]);
+    assert.deepEqual(take(array, 0), []);
+    assert.deepEqual(take(array, 2), [1, 7]);
+    assert.deepEqual(take(array, 100), [1, 7, -1, -100, 2]);
+  });
+
+  it('Массив с 10 элементами', () => {
+    const array = [1, 5, 8, -10, 2, 1, 0, 9, 1, 2];
+
+    assert.deepEqual(take(array), [1, 5, 8, -10, 2, 1, 0, 9, 1, 2]);
+    assert.deepEqual(take(array, 10), [1, 5, 8, -10, 2, 1, 0, 9, 1, 2]);
+    assert.deepEqual(take(array, 0), []);
+    assert.deepEqual(take(array, 5), [1, 5, 8, -10, 2]);
+    assert.deepEqual(take(array, 100), [1, 5, 8, -10, 2, 1, 0, 9, 1, 2]);
+  });
+
+  it('Тест на иммутабельность', () => {
+    const array = [1, 2, 3, 1, 2, 3];
+    take(array);
+    assert.deepEqual(array, [1, 2, 3, 1, 2, 3]);
   });
 });
