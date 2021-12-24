e9af680684edc12af23b1e96113826cdbb5548e7
“Radik <radik.zainullin@gmail.com>
2021-12-24 08:46:33 +0300

    задача решена

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-function sum(a, b) {
-  return a + b;
+// Функция getArithmeticProgressionSum получает в качестве аргументов:
+// a1 - значение первого член арифметической прогрессии
+// b - разность арифметической прогрессии
+// fromN - номер члена арифметической прогрессии,
+// с которого начинается искомая сумма
+// toN - номер члена арифметической прогрессии,
+// на котором заканчивается искомая сумма
+// Напишите функцию getArithmeticProgressionSum так,
+// чтобы она возвращала сумму членов арифметической прогрессии начиная
+// с fromN и заканчивая toN порядковыми номерами.
+
+function getArithmeticProgressionSum(a1, b, fromN, toN) {
+  const aFromN = a1 + (fromN - 1) * b;
+  const summa = ((2 * aFromN + b * (toN - fromN)) * (toN - fromN + 1)) / 2;
+  return summa;
 }
 
 module.exports = {
-  sum,
+  getArithmeticProgressionSum,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,15 @@
-const { assert } = require('chai');
+const { expect } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { getArithmeticProgressionSum } = require('../src');
 
 describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+  it("Тестирование функции getArithmeticProgressionSum", () => {
+    expect(getArithmeticProgressionSum(4, 3, 2, 4)).to.equal(30);
+    expect(getArithmeticProgressionSum(0, 1, 5, 10)).to.equal(39);
+    expect(getArithmeticProgressionSum(-2, 5, 2, 4)).to.equal(24);
+    expect(getArithmeticProgressionSum(-100, 105, 3, 6)).to.equal(1070);
+    expect(getArithmeticProgressionSum(5, 7, 12, 20)).to.equal(990);
+    expect(getArithmeticProgressionSum(1000, -99, 8, 22)).to.equal(-5790);
   });
 });
