9d3da1d7a59e26ed33854d057cdbb04ccd843941
“Radik <radik.zainullin@gmail.com>
2021-12-25 17:15:21 +0300

    задача решена

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-function sum(a, b) {
-  return a + b;
+// Выборка из массива take v2
+// Напишите функцию take, которая принимает массив и
+// возвращает новый массив, являющийся копией
+// исходного массива в обратном порядке. Функция должна оставить
+// исходный массив в исходном состоянии (обеспечить иммутабельность).
+// Не используйте методы массива, кроме push, shift, pop, unshift.
+
+function take(a) {
+  const inArr = a.slice();
+  const result = [];
+
+  while(inArr.length > 0) {
+    result.push(inArr.pop());
+  }
+  return result;
 }
 
 module.exports = {
-  sum,
+  take,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,42 @@
-const { assert } = require('chai');
+const { assert, expect } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { take } = require('../src');
 
-describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+describe('Тестирование функции take', () => {
+  it('Базовый тест', () => {
+    assert.deepEqual(take([]), []);
+    assert.deepEqual(take([1]), [1]);
+    assert.deepEqual(take([1, 2]), [2, 1]);
+    assert.deepEqual(take([1, 2, 3]), [3, 2, 1]);
+    assert.deepEqual(take([1, 2, 3, 3, 2, 1]), [1, 2, 3, 3, 2, 1]);
+  });
+
+  it('Пустой массив', () => {
+    const array = [];
+    const copy = take(array);
+
+    assert.deepEqual(copy, array);
+    expect(copy === array).to.equal(false);
+  });
+
+  it('Массив с 5 элементами', () => {
+    const array = [1, 7, -1, -100, 2];
+    const result = [2, -100, -1, 7, 1];
+
+    assert.deepEqual(take(array), result);
+  });
+
+  it('Массив с 10 элементами', () => {
+    const array = [1, 5, 8, -10, 2, 1, 0, 9, 1, 2];
+    const result = [2, 1, 9, 0, 1, 2, -10, 8, 5, 1];
+
+    assert.deepEqual(take(array), result);
+  });
+
+  it('Тест на иммутабельность', () => {
+    const array = [1, 2, 3, 1, 2, 3];
+    take(array);
+    assert.deepEqual(array, [1, 2, 3, 1, 2, 3]);
   });
 });
