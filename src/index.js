42fe4d6c56b3a735482e329c9747e045ba58afad
“Radik <radik.zainullin@gmail.com>
2021-12-27 12:20:44 +0300

    задача решена

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
-function sum(a, b) {
-  return a + b;
+// Перед выполнение этого задания рекомендовано ознакомиться с документацией
+// метод Array.prototype.includes:
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+// Напишите полифил метода Array.prototype.includes.
+// В теле полифила не используйте никаких методов класса Array или других классов.
+// Используйте только условные операторы и циклы.
+// В полифиле первый аргумент - это массив. Все остальные аргументы
+// такие же как и в том же порядке, что и у одноименного метода. Т.е.:
+// array.includes(item, fromIndex)  <=> includes(array, item, fromIndex)
+
+// https://school.constcode.ru/problem/solver/44
+
+// function includes(...rest) {
+//   const str = rest[0];
+//   const subStr = rest[1];
+//   const position = rest[2] === undefined ? 0 : rest[2];
+//   let pattern = new RegExp('\\b' + subStr, 'g');
+
+//   let matches = false;
+//   while (true) {
+//     matches = pattern.exec(str);
+//     if (matches === null) return false;
+//     if (matches.index >= position) return true;
+//   }
+// }
+
+// Метод includes() определяет, содержит ли массив определённый элемент,
+// возвращая в зависимости от этого true или false.
+ 
+function includes(...rest) {
+  const array = rest[0];
+  const item = rest[1];
+  const fromIndex = rest[2] === undefined ? 0 : rest[2];
+  let result = false;
+  array.forEach((el, i) => {
+    if (item === el && i > fromIndex) {
+      result = true;
+    }
+  });
+
+  return result;
 }
 
 module.exports = {
-  sum,
+  includes,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,63 @@
-const { assert } = require('chai');
+const { assert, expect } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { includes } = require('../src');
 
-describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+describe('Тестирование полифила Array.prototype.includes', () => {
+  it('Тест 1', () => {
+    const array = [1, 2, 3, 1, 6, 6, 0];
+
+    expect(includes(array, 1)).to.equal(true);
+    expect(array.includes(1)).to.equal(true);
+
+    expect(includes(array, 10)).to.equal(false);
+    expect(array.includes(10)).to.equal(false);
+
+    expect(includes(array, 'яблоко')).to.equal(false);
+    expect(array.includes('яблоко')).to.equal(false);
+
+    expect(includes(array, false)).to.equal(false);
+    expect(array.includes(false)).to.equal(false);
+
+    assert.deepEqual(array, [1, 2, 3, 1, 6, 6, 0]);
+  });
+
+  it('Тест 2', () => {
+    const array = [1, 2, 3, 1, 6, 6, 0];
+
+    expect(includes(array, 1, 2)).to.equal(true);
+    expect(array.includes(1, 2)).to.equal(true);
+
+    expect(includes(array, 1, 4)).to.equal(false);
+    expect(array.includes(1, 4)).to.equal(false);
+
+    expect(includes(array, 10, 2)).to.equal(false);
+    expect(array.includes(10, 2)).to.equal(false);
+
+    expect(includes(array, 'яблоко', 2)).to.equal(false);
+    expect(array.includes('яблоко', 2)).to.equal(false);
+
+    expect(includes(array, false, 2)).to.equal(false);
+    expect(array.includes(false, 2)).to.equal(false);
+
+    assert.deepEqual(array, [1, 2, 3, 1, 6, 6, 0]);
+  });
+
+  it('Тест 3', () => {
+    const array = [1, 2, 3, 1, 6, 6, 0];
+
+    expect(includes(array, 1, 100)).to.equal(false);
+    expect(array.includes(1, 100)).to.equal(false);
+
+    expect(includes(array, 10, 100)).to.equal(false);
+    expect(array.includes(10, 100)).to.equal(false);
+
+    expect(includes(array, 'яблоко', 100)).to.equal(false);
+    expect(array.includes('яблоко', 100)).to.equal(false);
+
+    expect(includes(array, false, 100)).to.equal(false);
+    expect(array.includes(false, 100)).to.equal(false);
+
+    assert.deepEqual(array, [1, 2, 3, 1, 6, 6, 0]);
   });
 });
