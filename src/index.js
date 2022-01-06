b06c4a2832babfbf1b84a8b3fbd0f66bf41740f4
“Radik <radik.zainullin@gmail.com>
2022-01-06 19:06:12 +0300

    задача решена

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-function sum(a, b) {
-  return a + b;
+// Напишите функцию высшего порядка getMap, которая принимает имя поля и возвращает функцию мапер.
+// Функция мапер принимает массив объектов и возвращает массив такой же длинны,
+// но содержащей значение объектов по переданному в getMap ключу.
+
+// const users = [
+//   { id: 1, name: "Алексей", surname: "Данчин" },
+//   { id: 2, name: "Сергей", surname: "Мельник" },
+//   { id: 3, name: "Дмитрий", surname: "Огго" },
+//   { id: 4, name: "Олег", surname: "Первый" },
+// ];
+
+// const getNames = getMap("name");
+
+// getNames(users); // ["Алексей", "Сергей", "Дмитрий", "Олег"]
+
+function getMap(str) {
+  return ((arr) => arr.map((el) => el[str]));
 }
 
 module.exports = {
-  sum,
+  getMap,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,32 @@
 const { assert } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { getMap } = require('../src');
 
-describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+describe('Тестирование функции getMap', () => {
+  it('Тест 1', () => {
+    const users = [
+      { id: 1, name: 'Алексей', surname: 'Данчин' },
+      { id: 2, name: 'Сергей', surname: 'Мельник' },
+      { id: 3, name: 'Дмитрий', surname: 'Огго' },
+      { id: 4, name: 'Олег', surname: 'Первый' },
+    ];
+
+    const getIds = getMap('id');
+    const getNames = getMap('name');
+    const getSurnames = getMap('surname');
+
+    assert.deepEqual(getIds(users), [1, 2, 3, 4]);
+
+    assert.deepEqual(getNames(users), ['Алексей', 'Сергей', 'Дмитрий', 'Олег']);
+
+    assert.deepEqual(getSurnames(users), ['Данчин', 'Мельник', 'Огго', 'Первый']);
+
+    assert.deepEqual(users, [
+      { id: 1, name: 'Алексей', surname: 'Данчин' },
+      { id: 2, name: 'Сергей', surname: 'Мельник' },
+      { id: 3, name: 'Дмитрий', surname: 'Огго' },
+      { id: 4, name: 'Олег', surname: 'Первый' },
+    ]);
   });
 });
