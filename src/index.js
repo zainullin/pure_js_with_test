77688ed7b8bd613a3b5fc63f9e87ea07d344a2ec
“Radik <radik.zainullin@gmail.com>
2022-01-08 22:58:20 +0300

    задача решена

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-function sum(a, b) {
-  return a + b;
+// Напишите функцию декоратор applyOnlyNumbers, которая принимает функцию и
+// возвращает новую функцию обертку. Функция обертка принимает любое количество аргументов,
+// но передает целевой функции только численные аргументы без NaN.
+// Пример:
+// const getSum = applyOnlyNumbers((...xs) =>
+//     xs.reduce((a, b) => a + b, 0)
+// );
+// getSum(1, 2, 3) // 1 + 2 + 3 = 6
+// getSum(1, "2", 3) // 1 + 3 = 4 ("2" - строка)
+// Примечание: можно пользоваться методом Array.prototype.filter.
+
+function applyOnlyNumbers(callback) {
+  return function(...args) {
+    const arr = args.filter(el => typeof el === 'number' && !isNaN(el));
+    return callback(...arr);
+  };
 }
 
 module.exports = {
-  sum,
+  applyOnlyNumbers,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,14 @@
-const { assert } = require('chai');
+const { expect } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { applyOnlyNumbers } = require('../src');
 
-describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+describe('Тестирование функции applyOnlyNumbers', () => {
+  it('Тест 1', () => {
+    const getSum = applyOnlyNumbers((...xs) => xs.reduce((a, b) => a + b, 0));
+    expect(getSum(1, 2, 3)).to.equal(6);
+    expect(getSum(1, '2', 3)).to.equal(4);
+    expect(getSum(NaN, 3)).to.equal(3);
+    expect(getSum(true, false)).to.equal(0);
   });
 });
