846e0371c6d48e0a3c54b87768de674f5b77df46
“Radik <radik.zainullin@gmail.com>
2022-01-09 13:25:45 +0300

    задача решена

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-function sum(a, b) {
-  return a + b;
+// Напишите декоратор callSeveral - которая принимает целевую функцию
+// и times - количество раз вызова функции и возвращает функцию обертку.
+// Функция обертка вызывает целевую функцию times раз со всеми своими
+// аргументами и возвращает массив результатов целевой функции.
+// const thrice = callSeveral((x) => x, 3);
+// thrice("Hello"); //["Hello", "Hello", "Hello"]
+
+function callSeveral(callback, times = 1) {
+  return function(args) {
+    const result = [];
+    if (times <= 0) {
+      return result;
+    }
+    for (let i = 0; i < times; i += 1) {
+      result.push(callback(args));
+    }
+    return result;
+  };
 }
 
 module.exports = {
-  sum,
+  callSeveral,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,23 @@
 const { assert } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { callSeveral } = require('../src');
 
-describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+describe('Тестирование функции callSeveral', () => {
+  it('Тест 1', () => {
+    const fun = callSeveral((x) => x, 3);
+    assert.deepEqual(fun('Hello'), ['Hello', 'Hello', 'Hello']);
+    assert.deepEqual(fun('world'), ['world', 'world', 'world']);
+    assert.deepEqual(fun('!'), ['!', '!', '!']);
+  });
+
+  it('Тест 2', () => {
+    let counter = 0;
+
+    const fun = callSeveral(() => counter++, 5);
+
+    assert.deepEqual(fun(), [0, 1, 2, 3, 4]);
+    assert.deepEqual(fun(), [5, 6, 7, 8, 9]);
+    assert.deepEqual(fun(), [10, 11, 12, 13, 14]);
   });
 });
