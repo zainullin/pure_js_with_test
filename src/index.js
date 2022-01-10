8ccea633759f67e8f56f6b22e2d1f0480da6e601
“Radik <radik.zainullin@gmail.com>
2022-01-10 11:50:08 +0300

    задача решена

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-function sum(a, b) {
-  return a + b;
+// Перед выполнение этого задания рекомендовано ознакомиться с документацией
+// метод Array.prototype.indexOf:
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+// Напишите полифил метода Array.prototype.indexOf. В теле полифила не используйте никаких методов
+// класса Array или других классов. Используйте только условные операторы и циклы.
+// В полифиле первый аргумент - это массив. Все остальные аргументы такие же как и в том же порядке,
+// что и у одноименного метода. Т.е.:
+// array.indexOf(item, fromIndex)  <=> indexOf(array, item, fromIndex)
+
+function indexOf(array, item, fromIndex = 0) {
+  for (let i = fromIndex; i < array.length; i += 1) {
+    if (item === array[i]) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 module.exports = {
-  sum,
+  indexOf,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,60 @@
-const { assert } = require('chai');
+const { assert, expect } = require('chai');
 const { describe, it } = require('mocha');
 
-const { sum } = require('../src');
+const { indexOf } = require('../src');
 
-describe('Main test', () => {
-  it('sum', () => {
-    assert.equal(sum(1, 1), 2);
+describe('Тестирование полифила Array.prototype.indexOf', () => {
+  it('Тест 1', () => {
+    const array = [1, 2, 3, 1, 6, 6, 0];
+    //  expect('Hello world!'.includes('Hello')).to.equal(true);
+    expect(indexOf(array, 1)).to.equal(0);
+    expect(array.indexOf(1)).to.equal(0);
+
+    expect(indexOf(array, 10)).to.equal(-1);
+    expect(array.indexOf(10)).to.equal(-1);
+
+    expect(indexOf(array, 'яблоко')).to.equal(-1);
+    expect(array.indexOf('яблоко')).to.equal(-1);
+
+    expect(indexOf(array, false)).to.equal(-1);
+    expect(array.indexOf(false)).to.equal(-1);
+
+    assert.deepEqual(array, [1, 2, 3, 1, 6, 6, 0]);
+  });
+
+  it('Тест 2', () => {
+    const array = [1, 2, 3, 1, 6, 6, 0];
+
+    expect(indexOf(array, 1, 2)).to.equal(3);
+    expect(array.indexOf(1, 2)).to.equal(3);
+
+    expect(indexOf(array, 10, 2)).to.equal(-1);
+    expect(array.indexOf(10, 2)).to.equal(-1);
+
+    expect(indexOf(array, 'яблоко', 2)).to.equal(-1);
+    expect(array.indexOf('яблоко', 2)).to.equal(-1);
+
+    expect(indexOf(array, false, 2)).to.equal(-1);
+    expect(array.indexOf(false, 2)).to.equal(-1);
+
+    assert.deepEqual(array, [1, 2, 3, 1, 6, 6, 0]);
+  });
+
+  it('Тест 3', () => {
+    const array = [1, 2, 3, 1, 6, 6, 0];
+
+    expect(indexOf(array, 1, 100)).to.equal(-1);
+    expect(array.indexOf(1, 100)).to.equal(-1);
+
+    expect(indexOf(array, 10, 100)).to.equal(-1);
+    expect(array.indexOf(10, 100)).to.equal(-1);
+
+    expect(indexOf(array, 'яблоко', 100)).to.equal(-1);
+    expect(array.indexOf('яблоко', 100)).to.equal(-1);
+
+    expect(indexOf(array, false, 100)).to.equal(-1);
+    expect(array.indexOf(false, 100)).to.equal(-1);
+
+    assert.deepEqual(array, [1, 2, 3, 1, 6, 6, 0]);
   });
 });
